6a5a2fa8815b9ea87501d05eee7828d190dbda2e
Jaser Uddin <udjaser@outlook.com>
2023-03-16 19:05:59 -0700

    initial commit

---
+++ server/event-planner/sequelize/migrations/20230310000000-create-tables.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        default: Sequelize.fn('uuid_generate_v4'),
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+    await queryInterface.createTable('Threads', {
+      id: {
+        allowNull: false,
+        default: Sequelize.fn('uuid_generate_v4'),
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      eventId: {
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+    await queryInterface.dropTable('Threads');
+  }
+};
---
+++ server/event-planner/src/config/env.ts
@@ -0,0 +1,3 @@
+require("dotenv").config();
+
+export const PORT = Number(process.env.PORT || 9000);
