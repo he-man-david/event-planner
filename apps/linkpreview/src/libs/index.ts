e03b0200b30d9fc41e49413e126bbf48ec55fabb
davidhe <davidhe88@pm.me>
2023-04-14 13:59:57 -0700

    wip link preview

---
+++ apps/linkpreview/src/libs/index.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { scrapeSite, SiteData, ScrapeOptions } from './scrape';
+
+interface ApiData {
+  success: boolean;
+  result?: SiteData;
+  error?: string;
+}
+
+const handler = async (req: Request, res: Response<ApiData>) => {
+  const { url } = req.body;
+  const scrapeOptions: ScrapeOptions | undefined = {
+    scrape: true,
+    stealth: true,
+  };
+  const { data, errors } = await scrapeSite(url, scrapeOptions);
+  if (data) {
+    res.send({ success: true, result: data });
+  }
+  res
+    .status(400)
+    .send({ success: false, result: data, error: JSON.stringify(errors) });
+};
+
+export default handler;
---
+++ apps/linkpreview/src/utils/common.ts
@@ -0,0 +1,60 @@
+import { sanitizeUrl } from '@braintree/sanitize-url';
+
+export const baseUrl =
+  process.env.SITE_URL || process.env.VERCEL_URL
+    ? process.env.SITE_URL
+      ? process.env.SITE_URL
+      : 'https://' + process.env.VERCEL_URL
+    : '';
+export const invalidUrlString = 'about:blank';
+
+export function isString(x: any): x is string {
+  return typeof x === 'string';
+}
+
+export const isValidWebUrl = (url: any) => {
+  if (sanitizeWebUrl(url) != invalidUrlString) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const sanitizeWebUrl = (url: any) => {
+  if (
+    isString(url) &&
+    (url.startsWith('https://') || url.startsWith('http://'))
+  ) {
+    return sanitizeUrl(url);
+  } else {
+    return invalidUrlString;
+  }
+};
+
+export const extractHostname = (url: string) => {
+  let hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+  if (url.indexOf('//') > -1) {
+    hostname = url.split('/')[2];
+  } else {
+    hostname = url.split('/')[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(':')[0];
+  //find & remove "?"
+  hostname = hostname.split('?')[0];
+
+  return hostname;
+};
+
+// export const stringToBoolParam = (inputParam: string | Array<string>) => {
+export const stringToBoolParam = (inputParam: any) => {
+  if (inputParam === 'true') {
+    return true;
+  } else if (inputParam === 'false') {
+    return false;
+  } else {
+    throw 'Parameter must be boolean string - "true" or "false"';
+  }
+};
