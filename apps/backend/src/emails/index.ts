4b3a8373169bdbbfbf958d9e26ac3439d6d7a49d
davidhe <davidhe88@pm.me>
2023-05-23 20:32:31 -0700

    fckkk

--- apps/backend/src/emails/index.ts
+++ apps/backend/src/emails/index.ts
@@ -7,10 +7,7 @@ const router = Router();
 router.post(
   '/',
   asyncHandler(async (req, res: Response<string>) => {
-    const result = await axios.post(
-      'https://oyster-app-64pps.ondigitalocean.app/emailservice',
-      req.body
-    );
+    const result = await axios.post('http:localhost:7070', req.body);
     res.send(result.data.message);
   })
 );
